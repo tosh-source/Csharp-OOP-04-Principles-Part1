cfa839552273e9ade52eb91064500c90413a9f82
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-03-05 23:53:54 +0200

    complete latest problem 3

--- 03_AnimalHierarchy/Tests.cs
+++ 03_AnimalHierarchy/Tests.cs
@@ -11,6 +11,23 @@ namespace _03_AnimalHierarchy
     {
         static void Main(string[] args)
         {
+            Dogs[] dogs =
+            {
+                new Dogs { Name = "Sharo", Sex = Sex.MALE.ToString(), Age = 4 },
+                new Dogs { Name = "Kevin", Sex = Sex.MALE.ToString(), Age = 2 },
+                new Dogs { Name = "Alex", Sex = Sex.FEMALE.ToString(), Age = 6 }
+            };
+
+            Console.WriteLine("Average age of all dogs: " + QueryOperations.AverageAge(dogs));
+
+            Kittens[] kittens =
+            {
+                new Kittens { Name = "Venom", Sex = Sex.MALE.ToString(), Age = 200 },
+                new Kittens { Name = "Kim", Sex = Sex.FEMALE.ToString(), Age = 5 },
+                new Kittens { Name = "Abi", Sex = Sex.FEMALE.ToString(), Age = 9 }
+            };
+
+            Console.WriteLine("Average age of all kittens: " + QueryOperations.AverageAge(kittens));
         }
     }
 }
