86efbfab87d6ead11e6d9159312a145d9e1a70d3
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-03-05 02:33:50 +0200

    add query operation static class

---
+++ 03_AnimalHierarchy/Models/QueryOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_AnimalHierarchy.Models
+{
+    class QueryOperations
+    {
+        public static double AverageAge(params Animals[] animals)
+        {
+            var temp = animals.Select(animal => animal.Age).ToList();
+            var result = temp.Average();
+
+            return result;
+        }
+    }
+}
