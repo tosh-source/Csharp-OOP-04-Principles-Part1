db6b0960f0f66284c7e489f2cc903a38d1367df1
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-03-05 02:31:25 +0200

    move classes files

---
+++ 03_AnimalHierarchy/Models/Animals/Animals.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_AnimalHierarchy.Models
+{
+    abstract class Animals : ISound
+    {
+        private int age;
+        private string name;
+        private string sex;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Sex
+        {
+            get { return sex; }
+            set { sex = value; }
+        }
+
+        public abstract void Sound();
+    }
+}
---
+++ 03_AnimalHierarchy/Models/Animals/Tomcat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_AnimalHierarchy.Models
+{
+    class Tomcat : Cats
+    {
+        public const Sex tomcatSex = Models.Sex.MALE;
+
+        public override void Sound()
+        {
+            Console.WriteLine("Tomcat say miauuu..");
+            //base.Sound();
+        }
+    }
+}
